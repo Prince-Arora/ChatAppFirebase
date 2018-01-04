4b29a3c19edbbeb1b869abe02081968e564b2315
Prince-Arora <19princearora95@gmail.com>
2018-01-04 21:05:33 +0530

    4th

--- app/src/main/java/com/example/laptop/chatappfirebase/MainActivity.java
+++ app/src/main/java/com/example/laptop/chatappfirebase/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.laptop.chatappfirebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+ private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       if(currentUser == null )
+       {
+           Intent startactivity=new Intent(MainActivity.this,StartActivity.class);
+           startActivity(startactivity);
+           finish();
+       }
     }
 }
